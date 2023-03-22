91867a0af12830ac91aacc8b540eb6e90667fe14
Suman Shrestha <smnxrstt39@gmail.com>
2023-03-22 11:02:41 +0545

    start of cs journey

---
+++ hellostrings.cs
@@ -0,0 +1,12 @@
+Console.WriteLine("hello world ");
+string aFriend = "suman";
+Console.WriteLine(aFriend);
+Console.WriteLine("hello world"+ aFriend);
+//string interpolation
+Console.WriteLine($"hello {aFriend}");
+//strings and strings
+string firstFriend = "Pass";
+string secondFried = "Chu ji la";
+Console.WriteLine($"My friend how {firstFriend} abd {secondFried}");
+
+Console.WriteLine($"the name {firstFriend} has {firstFriend.Length} letters");
